0ae4e5e3a27bcded4065d3b7dfdb736e1b74b184
Artem Beydin <artbeidin.exceedteam@gmail.com>
2021-10-22 16:14:35 +0300

    code style

--- script.js
+++ script.js
@@ -7,24 +7,23 @@ window.onload = init = async () => {
     input.addEventListener('change', updateValue);
     const resp = await fetch('http://localhost:8000/allTasks', {
         method: 'GET'
-    })
+    });
     let result = await resp.json();
     allTasks = result.data;
     render();
-
-}
+};
 
 const onClickDelete = () => {
     allTasks = [];
     localStorage.setItem('tasks', JSON.stringify(allTasks)); 
     render();
-}
+};
 
 const onClickButton = async () => {
     allTasks.push({
         text: valueInput,
         isCheck: false
-    })
+    });
     const resp = await fetch('http://localhost:8000/createTask', {
         method: 'POST',
         headers: {
@@ -35,73 +34,70 @@ const onClickButton = async () => {
             text: valueInput,
             isCheck: false
         })
-    })
+    });
     let result = await resp.json();
     allTasks = result.data;
     console.log(allTasks);
     valueInput = '';
     input.value = '';
     render();
-}
+};
 
 const updateValue = (event) => {
 valueInput = event.target.value;
-}
+};
 
 const render = () => {
     const content = document.getElementById('content-page');
     while (content.firstChild) {
         content.removeChild(content.firstChild);
-    }
-allTasks.map((item, index) => {
+    };
+    allTasks.map((item, index) => {
     
-    const container = document.createElement('div');
-    container.id = `task-${index}`;
-    container.className = 'task-container';
-    const checkBox = document.createElement('input');
-    checkBox.type = 'checkbox';
-    checkBox.checked = item.isCheck;
-    checkBox.onchange = function () {
-        onChangeCheckbox(index);
-    }
-    container.appendChild(checkBox);
-    const text = document.createElement('p');
+        const container = document.createElement('div');
+        container.id = `task-${index}`;
+        container.className = 'task-container';
+        const checkBox = document.createElement('input');
+        checkBox.type = 'checkbox';
+        checkBox.checked = item.isCheck;
+        checkBox.onchange = function () {
+            onChangeCheckbox(index);
+        };
 
-    text.innerText = item.text;
-    
-    text.className = item.isCheck ? 'text-task done-text' : 'text-task';
-    container.appendChild(text);
-    const imageEdit = document.createElement('i');
-    imageEdit.className = 'far fa-edit';
-    container.appendChild(imageEdit);
-    const test = item.text;
-    imageEdit.onclick = () => {
-        imageEdit.className = 'far fa-check-square';
-        imageDelete.className = 'fas fa-backspace';
-        const inputTask = document.createElement('input');
-        inputTask.value = text.innerText;
-        container.replaceChild(inputTask, text);
-        
-        imageEdit.onclick = () => {             
-            editVal(text, inputTask, container, item);         
-        }
-        imageDelete.onclick = () => {
-            inputTask.value = test;         
-            render();
-        }
-        
-       
-        }
+        container.appendChild(checkBox);
+        const text = document.createElement('p');
+        text.innerText = item.text;        
+        text.className = item.isCheck ? 'text-task done-text' : 'text-task';
+        container.appendChild(text);
+        const imageEdit = document.createElement('i');
+        imageEdit.className = 'far fa-edit';
+        container.appendChild(imageEdit);
+        const test = item.text;
+        imageEdit.onclick = () => {
+            imageEdit.className = 'far fa-check-square';
+            imageDelete.className = 'fas fa-backspace';
+            const inputTask = document.createElement('input');
+            inputTask.value = text.innerText;
+            container.replaceChild(inputTask, text);            
+            imageEdit.onclick = () => {             
+                editVal(text, inputTask, container, item);         
+            };
+
+            imageDelete.onclick = () => {
+                inputTask.value = test;         
+                render();
+            };
+        };
+
     const imageDelete = document.createElement('i');
     imageDelete.className = 'far fa-trash-alt';
     container.appendChild(imageDelete);
     imageDelete.onclick = () => {
       deleteVal(index);  
-    }
+    };
     content.appendChild(container);
-}
-)
-}
+    });
+};
 
     const editVal = (text, inputTask, container, item) => {
         text.innerText = inputTask.value;
@@ -109,11 +105,9 @@ allTasks.map((item, index) => {
         container.replaceChild(text, inputTask);
         localStorage.setItem('tasks', JSON.stringify(allTasks)); 
         render();
-    }
+    };
 
     const deleteVal = async (index) => {
-    // allTasks = allTasks.filter((item, index1) => (index1 !== index));   
-    
     const resp = await fetch(`http://localhost:8000/deleteTask?id=${allTasks[index].id}`, {
         method: 'DELETE',
         headers: {
@@ -124,7 +118,7 @@ allTasks.map((item, index) => {
             text: valueInput,
             isCheck: false
         })
-    })
+    });
     let result = await resp.json();
     allTasks = result.data;
     render();
@@ -134,4 +128,4 @@ const onChangeCheckbox = (index) => {
 allTasks[index].isCheck = !allTasks[index].isCheck;
 localStorage.setItem('tasks', JSON.stringify(allTasks)); 
 render();
-}
+};
